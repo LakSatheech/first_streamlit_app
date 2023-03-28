0d016add3f38df0c4494fd0d3533efa4b0921aa1
LakSatheech <129170150+LakSatheech@users.noreply.github.com>
2023-03-28 14:54:46 +0100

    Update streamlit_app.py
    added breakfast menu header and 3 items with empjis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title ('My Parents New Healthy Diner')
 
 streamlit.header ('Breakfast menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket smoothie')
-streamlit.text('Hard-Boiled Free-range Egg')
+streamlit.text('🥣Omega 3 & Blueberry oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket smoothie')
+streamlit.text('🐔Hard-Boiled Free-range Egg')
+streamlit.text('🥑🍞Avocado Toast')
